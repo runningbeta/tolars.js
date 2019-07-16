807dc6af2325b53cad8fcf18526105cab3980cba
Kristijan Rebernisak <kristijan.rebernisak@gmail,com>
2019-07-16 20:17:28 +0200

    Add getEthereumAddress method to API

--- src/index.js
+++ src/index.js
@@ -4,6 +4,43 @@ const TOLAR_ADDRESS_CHAR_LENGTH = 50;
 const TOLAR_ADDRESS_CHECKSUM_CHAR_LENGTH = 8;
 const TOLAR_ADDRESS_CHECKSUM_START_INDEX = 42;
 const TOLAR_ADDRESS_PREFIX = '54';
+
+/**
+ * The address is 25 bytes long.
+ *
+ * First byte being 0x54 ('T').
+ *
+ * @param {Tolar address} address
+ */
+const checkAddressShape = (address) => {
+  if (typeof (address) !== 'string') throw new Error('Invalid address.');
+  if (address.length !== TOLAR_ADDRESS_CHAR_LENGTH) throw new Error('Address wrong length. Should be 25 bytes or 50 characters long.');
+  if (!address.startsWith(TOLAR_ADDRESS_PREFIX)) throw new Error('Address should start with "54" as byte representation of "T"');
+};
+
+/**
+ * The address is 25 bytes long.
+ *
+ * First byte being 0x54 ('T').
+ *
+ * Next 20 bytes are the Ethereum address of the public key (right 160 bits of sha3 of pub_key).
+ *
+ * @param {Ethereum address} address
+ */
+const getEthereumAddressSubstring = address => ethers.utils.getAddress(`0x${address.substring(2, TOLAR_ADDRESS_CHECKSUM_START_INDEX)}`);
+
+/**
+ * The address is 25 bytes long.
+ *
+ * First byte being 0x54 ('T').
+ *
+ * Next 20 bytes are the Ethereum address of the public key (right 160 bits of sha3 of pub_key).
+ *
+ * @param {Ethereum address} address
+ */
+const getEthereumAddress = address => checkAddressShape(address)
+  || getEthereumAddressSubstring(address);
+
 /**
  * The Ethereum address is 20 bytes long.
 .*
@@ -21,11 +58,24 @@ const getChecksum = (address) => {
 };
 
 /**
+ * The Ethereum address is 20 bytes long.
+.*
+ * The checksum is calculated by taking the last 4 bytes of the sha3(sha3(20bytes-addr)).
+ *
+ * @param {Ethereum address} address
+ */
+const checkAddressChecksum = (address) => {
+  const ethAddress = getEthereumAddressSubstring(address);
+  const checksum = address.substring(TOLAR_ADDRESS_CHECKSUM_START_INDEX, TOLAR_ADDRESS_CHAR_LENGTH);
+  if (checksum !== getChecksum(ethAddress)) throw new Error('Invalid address checksum');
+};
+
+/**
  * The address is 25 bytes long.
  *
  * First byte being 0x54 ('T').
  *
- * Next 20 bytes are the eth address of the public key (right 160 bits of sha3 of pub_key).
+ * Next 20 bytes are the Ethereum address of the public key (right 160 bits of sha3 of pub_key).
  *
  * The last 4 bytes are the checksum of the previous 20 byte.
  * The checksum is calculated by taking the last 4 bytes of the sha3(sha3(20bytes-addr)).
@@ -36,14 +86,9 @@ const getChecksum = (address) => {
  * @param {Tolar address} address
  */
 const getAddress = (address) => {
-  if (typeof (address) !== 'string') throw new Error('Invalid address.');
-  if (address.length !== TOLAR_ADDRESS_CHAR_LENGTH) throw new Error('Address wrong length. Should be 25 bytes or 50 characters long.');
-  if (!address.startsWith(TOLAR_ADDRESS_PREFIX)) throw new Error('Address should start with "54" as byte representation of "T"');
-  const ethAddress = ethers.utils.getAddress(`0x${address.substring(2, TOLAR_ADDRESS_CHECKSUM_START_INDEX)}`);
-  const checksum = address.substring(TOLAR_ADDRESS_CHECKSUM_START_INDEX, TOLAR_ADDRESS_CHAR_LENGTH);
-  const calculatedChecksum = getChecksum(ethAddress);
-  if (checksum !== calculatedChecksum) throw new Error('Invalid address checksum');
-  return `${TOLAR_ADDRESS_PREFIX}${ethAddress.substring(2).toLowerCase()}${checksum}`;
+  checkAddressShape(address);
+  checkAddressChecksum(address);
+  return address;
 };
 
 const isAddress = (address) => {
@@ -53,4 +98,4 @@ const isAddress = (address) => {
   return true;
 };
 
-export { getAddress, isAddress };
+export { getEthereumAddress, getAddress, isAddress };

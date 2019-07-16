1b908c057f0285e8b6217a80cf59f8a993580d9c
Kristijan Rebernisak <kristijan.rebernisak@gmail,com>
2019-07-16 12:52:18 +0200

    Add ethers.js dependency to check Ethereum address validity, and add more basic tests.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
+import { ethers } from 'ethers';
+
+const TOLAR_ADDRESS_CHAR_LENGTH = 50;
+const TOLAR_ADDRESS_CHECKSUM_START_INDEX = 42;
+const TOLAR_ADDRESS_PREFIX = '54';
+
+/**
+ * The address is 25 bytes long.
+ *
+ * First byte being 0x54 ('T').
+ *
+ * Next 20 bytes are the eth address of the public key (right 160 bits of sha3 of pub_key).
+ *
+ * The last 4 bytes are the checksum of the previous 20 byte.
+ * The checksum is calculated by taking the last 4 bytes of the sha3(sha3(20bytes-addr)).
+ *
+ * Example of a valid tolar address: 54948c78114bc39675157e097830ae63c0da7857a19c13aec7
+ * Note how the address starts with "54" since it is the byte representation of "T".
+ *
+ * @param {Tolar address} address
+ */
 const getAddress = (address) => {
-  if (!address) throw new Error('Address is null');
-  if (!address.startsWith('54')) throw new Error('Address should start with "54"');
-  // TODO: checksum
-  return address;
+  if (typeof (address) !== 'string') throw new Error('Invalid address.');
+  if (address.length !== TOLAR_ADDRESS_CHAR_LENGTH) throw new Error('Address wrong length. Should be 25 bytes or 50 characters long.');
+  if (!address.startsWith(TOLAR_ADDRESS_PREFIX)) throw new Error('Address should start with "54" as byte representation of "T"');
+  const ethAddress = ethers.utils.getAddress(`0x${address.substring(2, TOLAR_ADDRESS_CHECKSUM_START_INDEX)}`);
+  const checksum = address.substring(TOLAR_ADDRESS_CHECKSUM_START_INDEX, TOLAR_ADDRESS_CHAR_LENGTH);
+  // TODO: check checksum
+  return `${TOLAR_ADDRESS_PREFIX}${ethAddress.substring(2).toLowerCase()}${checksum}`;
 };
 
 const isAddress = (address) => {
--- test/index.js
+++ test/index.js
@@ -1,8 +1,37 @@
-import { assert } from 'chai';
-import { isAddress } from '../src';
+import { assert, expect } from 'chai';
+import { getAddress, isAddress } from '../src';
 
-describe('Awesome test.', () => {
-  it('should test isAddress function returns false', () => {
+describe('Address test.', () => {
+  it('should test getAddress function throws on null', () => {
+    expect(() => getAddress()).to.throw();
+  });
+
+  it('should test getAddress function throws on hex number', () => {
+    const val = 0x54948c78114bc39675157e097830ae63c0da7857a19c13aec7;
+    expect(() => getAddress(val)).to.throw();
+  });
+
+  it('should test getAddress function throws on empty string', () => {
+    const val = '';
+    expect(() => getAddress(val)).to.throw();
+  });
+
+  it('should test getAddress function throws on string length !== 50', () => {
+    const val = '54948c78114bc396751';
+    expect(() => getAddress(val)).to.throw();
+  });
+
+  it('should test getAddress function throws for Ethereum address', () => {
+    const val = '0x7b00ae36c7485b678fe945c2dd9349eb5baf7b6b';
+    expect(() => getAddress(val)).to.throw();
+  });
+
+  it('should test getAddress function returns address', () => {
+    const expectedVal = '54948c78114bc39675157e097830ae63c0da7857a19c13aec7';
+    assert(getAddress(expectedVal) === expectedVal, 'Test failed :(');
+  });
+
+  it('should test isAddress function returns false for Ethereum address', () => {
     const expectedVal = false;
     assert(isAddress('0x7b00ae36c7485b678fe945c2dd9349eb5baf7b6b') === expectedVal, 'Test failed :(');
   });

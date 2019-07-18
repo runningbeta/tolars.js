a8d544e8658be5e3b4999e76c01909fac44905c4
Kristijan Rebernisak <kristijan.rebernisak@gmail,com>
2019-07-18 10:46:29 +0200

    Improve lib JSDoc

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ const TOLAR_ADDRESS_CHECKSUM_START_INDEX = 42;
 const TOLAR_ADDRESS_PREFIX = '54';
 
 /**
- * The address is 25 bytes long.
+ * Check if Tolar address string has correct shape.
  *
- * First byte being 0x54 ('T').
+ * The address should be 25 bytes long, first byte being 0x54 ('T').
  *
- * @param {Tolar address} address
+ * @param {string} Tolar address
+ * @throws {Error} invalid Tolar address shape
  */
 const checkAddressShape = (address) => {
   if (typeof (address) !== 'string') throw new Error('Invalid address.');
@@ -25,7 +26,8 @@ const checkAddressShape = (address) => {
  *
  * Next 20 bytes are the Ethereum address of the public key (right 160 bits of sha3 of pub_key).
  *
- * @param {Ethereum address} address
+ * @param {string} Tolar address
+ * @return {string} Ethereum address
  */
 const getEthereumAddressSubstring = address => ethers.utils.getAddress(`0x${address.substring(2, TOLAR_ADDRESS_CHECKSUM_START_INDEX)}`);
 
@@ -36,17 +38,21 @@ const getEthereumAddressSubstring = address => ethers.utils.getAddress(`0x${addr
  *
  * Next 20 bytes are the Ethereum address of the public key (right 160 bits of sha3 of pub_key).
  *
- * @param {Ethereum address} address
+ * @param {string} Tolar address
+ * @return {string} Ethereum address
+ * @throws {Error} invalid address shape
  */
 const getEthereumAddress = address => checkAddressShape(address)
-  || getEthereumAddressSubstring(address);
+  && getEthereumAddressSubstring(address);
 
 /**
- * The Ethereum address is 20 bytes long.
+ * Generate Tolar address checksum from Ethereum address.
 .*
  * The checksum is calculated by taking the last 4 bytes of the sha3(sha3(20bytes-addr)).
  *
- * @param {Ethereum address} address
+ * @param {string} Tolar address
+ * @return {string} Tolar address checksum generated from Ethereum address
+ * @throws {Error} invalid Ethereum address
  */
 const getChecksum = (address) => {
   const hash = ethers.utils.keccak256;
@@ -58,23 +64,29 @@ const getChecksum = (address) => {
 };
 
 /**
- * Generate Tolar address from Ethereum address
+ * Generate Tolar address from Ethereum address.
  *
- * @param {Ethereum address} address
+ * @param {string} Ethereum address
+ * @return {string} Tolar address
+ * @throws {Error} invalid Ethereum address
  */
 const fromEthereumAddress = (address) => {
-  const ethAddress = ethers.utils.getAddress(address);
-  const ethAddressNormalized = ethAddress.toLowerCase().substring(2);
+  const addressNormalized = ethers.utils.getAddress(address)
+    .toLowerCase()
+    .substring(2);
   const checksum = getChecksum(address);
-  return `${TOLAR_ADDRESS_PREFIX}${ethAddressNormalized}${checksum}`;
+  return `${TOLAR_ADDRESS_PREFIX}${addressNormalized}${checksum}`;
 };
 
 /**
+ * Check if Tolar address string has correct checksum.
+ *
  * The Ethereum address is 20 bytes long.
-.*
  * The checksum is calculated by taking the last 4 bytes of the sha3(sha3(20bytes-addr)).
  *
- * @param {Ethereum address} address
+ * @param {string} Tolar address
+ * @throws {Error} invalid Ethereum address
+ * @throws {Error} invalid address checksum
  */
 const checkAddressChecksum = (address) => {
   const ethAddress = getEthereumAddressSubstring(address);
@@ -95,7 +107,10 @@ const checkAddressChecksum = (address) => {
  * Example of a valid tolar address: 54948c78114bc39675157e097830ae63c0da7857a19c13aec7
  * Note how the address starts with "54" since it is the byte representation of "T".
  *
- * @param {Tolar address} address
+ * @param {string} Tolar address
+ * @throws {Error} invalid Tolar address shape
+ * @throws {Error} invalid Ethereum address
+ * @throws {Error} invalid address checksum
  */
 const getAddress = (address) => {
   checkAddressShape(address);
@@ -103,6 +118,12 @@ const getAddress = (address) => {
   return address;
 };
 
+/**
+ * Check if string is valid Tolar address
+ *
+ * @param {string} Tolar address
+ * @return {boolean} Tolar address validity
+ */
 const isAddress = (address) => {
   try {
     getAddress(address);
